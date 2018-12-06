8fa7288351f76c1ab720d50ef68ca9720d4d8c87
Gang ZHANG <gangz@emergentdesign.cn>
2018-12-06 11:21:18 +0800

    refactory

---
+++ src/test/java/depends/extractor/java/JavaParserTest.java
@@ -0,0 +1,21 @@
+package depends.extractor.java;
+
+import depends.entity.Inferer;
+import depends.entity.repo.EntityRepo;
+import depends.extractor.java.JavaBuiltInType;
+import depends.extractor.java.JavaFileParser;
+import depends.extractor.java.JavaImportLookupStrategy;
+
+public abstract class JavaParserTest {
+	protected  EntityRepo entityRepo ;
+	protected Inferer inferer ;
+
+	public void init() {
+		entityRepo = new EntityRepo();
+		inferer = new Inferer(entityRepo,new JavaImportLookupStrategy(),new JavaBuiltInType());
+	}
+	
+	public JavaFileParser createParser(String src) {
+		return new JavaFileParser(src,entityRepo, inferer);
+	}
+}
